2be31d342df477cd75690b9d7ffe70b6ca5c1a05
DapperSlothis <ewfinke@mtu.edu>
2026-02-12 10:10:30 -0500

    Rename class from 'ello' to 'Hello'

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 // Hello.java
 // Compile: javac Hello.java
 // Run: java Hello
-public class ello {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
